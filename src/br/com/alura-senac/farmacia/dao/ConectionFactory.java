c2eb24eeaddbe93547c8fa6ead02be69d05a1992
Ariel Carvalho <ariell_carvalho@hotmail.com>
2024-02-26 07:59:30 -0300

    8 - Criar ConnectionFactory

---
+++ src/br/com/alura-senac/farmacia/dao/ConectionFactory.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectionFactory
+{
+    public Connection recuperarConexao()
+    {
+        try
+        {
+            return DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/farmacia?user=root&password=root");
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/br/com/alura-senac/farmacia/modelo/Produto.java
@@ -0,0 +1,34 @@
+public class Produto
+{
+    private int id;
+    private float preco;
+    private String nome;
+    private String fabricante;
+
+    public Produto(float preco, String nome, String fabricante)
+    {
+        this.preco = preco;
+        this.nome = nome;
+        this.fabricante = fabricante;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public float getPreco()
+    {
+        return preco;
+    }
+
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public String getFabricante()
+    {
+        return fabricante;
+    }
+}
